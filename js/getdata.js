cd8db278c67088474f75c4c04b3df32ff67f83d9
Karthik <msvsk1997@gmail.com>
2017-10-14 19:52:11 +0530

    cookie

--- js/getdata.js
+++ js/getdata.js
@@ -4,20 +4,32 @@ $(document).ready(function(){
         var titleID = document.getElementById("titleID").value.toUpperCase;
         var companyID = document.getElementById("companyID").value.toUpperCase;
         
-        var cookies = document.cookie;
+        /*var cookies = document.cookie;
         cookarg = cookies.split(';');
         mail = cookarg[0].split('=')[1];
-        name = cookarg[1].split('=')[1];
+        name = cookarg[1].split('=')[1];*/
+        
+        firebase.auth().onAuthStateChanged(function(user){
+            if(user)
+                {
+                    mail = user.email;
+                }
+            else 
+                {
+                    console.log("no user exists");
+                }
+        }
               
-        var d = new Date();
-        var ts = d.getTime();
-        document.getElementById("data-container").innerHTML = content;
+        //var d = new Date();
+        //var ts = d.getTime();
+        document.getElementById("data-container").innerHTML = mail;
         /*var experience = {
             timestamp: ts,
             username: name,
-            email: emailID
+            email: mail,
             title: titleID,
-         */
+            company: companyID
+        }*/
         //document.getElementById("data-container").innerHTML = experience;
         return false;
     });
