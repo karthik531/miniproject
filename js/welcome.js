657c80eda1a837d11d6e9c58df9a6f2af449487a
WebDevJntu <miniprojectjntu@gmail.com>
2017-12-01 19:25:11 +0530

    Q/A done without styling. changes done in welcome.html and welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,7 @@
 window.onload = function()
 {
     initApp();
+    firstClick = true;
 };
 
 function handleSignOut()
@@ -14,23 +15,123 @@ function displayEditor()
     document.getElementById("editor").style.display = "block";
 }
 
+function displaySupporter(x)
+{
+    if(x=="none" && firstClick)
+       firstClick = false;
+    else if(x=="none" && !firstClick)
+        document.getElementById(presentId).style.display = "none";
+    
+}
+
 function insertEditor()
 {
 //document.getElementById("loader").style.display = "block";
 //document.getElementById("editor").innerHTML='<object onload="displayEditor()" width="75%" height="100%" type="text/html" data="editor.html"    //</object>';
-    
-    document.getElementById("cards").style.display = "none";
-    document.getElementById("cardcontent").style.display = "none";
+    var x = document.getElementById("editor").style.display;
+    displaySupporter(x);
     document.getElementById("editor").style.display = "block";
-  
+    presentId = "editor";
+}  
+
+
+function insertQuestion()
+{
+    var question = document.getElementById("questionbox").value;
+    if(question=="") 
+    {
+        alert("EMPTY QUESTION NOT ACCEPTED");
+        return;
+    }
+    var user_name = document.getElementById("und").innerHTML;
+    var questionData = {username : user_name , question: question,views: 0};
+    var promise = firebase.firestore().collection("questions").doc().set(questionData);
+    
+}
+
+function getQuestions()
+{
+    var question_path = firebase.firestore().collection("questions");
+   
+    question_path.onSnapshot(function(querySnapshot) 
+    {
+         var questionString = "";
+            querySnapshot.forEach(function(doc) 
+            { 
+
+                questionString = questionString+'<div id ="ind-question" onclick=getQuestionContent("'+doc.id+'")>'+
+                '<span id="indquestion">'+doc.data().question+'</span>'+
+                '<span id="user-name">'+doc.data().username+'</span>'+
+                '</div>'
+            });
+        
+            var x = document.getElementById("questions").style.display;
+            displaySupporter(x);
+            document.getElementById("questions").style.display = "block";
+            presentId = "questions";
+            document.getElementById("questionlist").innerHTML = questionString;
+    });
+} 
+
+function getQuestionContent(docId)
+{
+    document.getElementById("questions").style.display = "none";
+    document.getElementById("questioncontent").style.display = "block";
+    presentId = "questioncontent";
+    var questionpath = firebase.firestore().collection("questions").doc(docId);
+    
+    answerpath = questionpath.collection("comments");
+    
+    questionpath.get().then(function(doc)
+    {
+        if(doc && doc.exists)
+        {
+    
+            document.getElementById("question").innerHTML = doc.data().question;
+        }
+    });
+    getAllAnswers();
+
+}
+function getAllAnswers()
+{
+    answerpath.onSnapshot(function(querySnapshot) 
+    {
+        var answerString = "";
+        
+        querySnapshot.forEach(function(doc) 
+        {
+            username = doc.data().username;
+            comment = doc.data().comment;
+            answerString += "<p id='ausername'>"+ username +"</p>"
+            answerString+="<p id='answer'>"+ comment +"</p>"    
+        });
+        
+        document.getElementById("answers").innerHTML = answerString;
+    });
+}
+function insertAnswer()
+{
+    var answer = document.getElementById("answer-box").value;
+    if(answer=="")
+    {
+        alert("empty answer not accepted");
+    }
+    else
+    {
+        var user_name = document.getElementById("und").innerHTML;
+        var answerData = {username : user_name , comment : answer};
+        var promise = answerpath.doc().set(answerData);
+    }
 }
 
 function getInterviewExperiences()
 {
 	path = firebase.firestore().collection("posts");
-    var docIdString = "";
-    path.get().then(function(querySnapshot) 
+   
+    path.onSnapshot(function(querySnapshot) 
     {
+         var docIdString = "";
             querySnapshot.forEach(function(doc) 
             { 
 
@@ -40,9 +141,10 @@ function getInterviewExperiences()
                 '</div>'
             });
         
+            var x = document.getElementById("cards").style.display;
+            displaySupporter(x);
             document.getElementById("cards").style.display = "block";
-            document.getElementById("editor").style.display = "none";
-            document.getElementById("cardcontent").style.display = "none";
+            presentId = "cards";
             document.getElementById("cards").innerHTML = docIdString;
     });
 }
@@ -50,20 +152,29 @@ function getInterviewExperiences()
 function gotoCards()
 {
     document.getElementById("cards").style.display = "block";
+    presentId = "cards";
     document.getElementById("cardcontent").style.display = "none";
-    
+}
+
+function gotoQuestions()
+{
+    document.getElementById("questions").style.display = "block";
+    presentId = "questions";
+    document.getElementById("questioncontent").style.display = "none";
 }
 
 function getAllContent(docId)
 {
     document.getElementById("cards").style.display = "none";
     document.getElementById("cardcontent").style.display = "block";
+    presentId = "cardcontent";
+   
     document.getElementById("description").innerHTML = "";
     document.getElementById("comments").innerHTML = "";
     
     var descriptionPath = path.doc(docId);
     
-    commentpath = path.doc(docId).collection("comments");
+    commentpath = descriptionPath.collection("comments");
     
     descriptionPath.get().then(function(doc)
     {
@@ -74,6 +185,21 @@ function getAllContent(docId)
     });
 
    getAllComments();
+}
+
+function insertComment()
+{
+    var com = document.getElementById("comment-box").value;
+    if(com=="")
+    {
+        alert("empty comment not accepted");
+    }
+    else
+    {
+        var user_name = document.getElementById("und").innerHTML;
+        var commentData = {username : user_name , comment : com};
+        var promise = commentpath.doc().set(commentData);
+    }
 }
 
 function getAllComments()
@@ -96,18 +222,6 @@ function getAllComments()
     });
 }
 
-function insertComment()
-{
-    var com = document.getElementById("comment-box").value;
-    if(com==""){
-        alert("empty comment not accepted");
-    }
-    else{
-           var user_name = document.getElementById("und").innerHTML;
-           var commentData = {username : user_name , comment : com};
-        var promise = commentpath.doc().set(commentData);
-    }
-}
 function getName()
 {
 	if(user_ref.displayName==null)
@@ -139,6 +253,7 @@ function getName()
 	}
 	document.getElementById("ued").innerHTML = user_ref.email;
 }
+
 function initApp()
 {
     firebase.auth().onAuthStateChanged(function(user)
